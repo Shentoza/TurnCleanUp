6521eadd2070e0b559a5237d27ce800a28b36a78
Skeletti <schreiber.daniel87@gmail.com>
2016-08-25 16:56:42 +0200

    ObjectBar implemented

--- Assets/Scripts/UI/Daniel UI/UIEditorManager.cs
+++ Assets/Scripts/UI/Daniel UI/UIEditorManager.cs
@@ -1,36 +1,75 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class UIEditorManager : MonoBehaviour {
 
     Button unitB, objectB, brushB, leftRotateB, rightRotateB, removeB;
+    GameObject assetBar, objectBar;
+    List<GameObject> objectButtons = new List<GameObject>();
+    List<GameObject> objectPrefabs;
 
-	// Use this for initialization
-	void Start () {
-        unitB = GameObject.Find("UnitB").GetComponent<Button>();
-        unitB.onClick.AddListener(() => Methode());
+    // Use this for initialization
+    void Start () {
+        ObjectSetterHelperLE osh = GameObject.Find("Plane").GetComponent<ObjectSetterHelperLE>();
+       // unitB = GameObject.Find("UnitB").GetComponent<Button>();
+       // unitB.onClick.AddListener(() => Methode());
 
         objectB = GameObject.Find("ObjectB").GetComponent<Button>();
-        objectB.onClick.AddListener(() => Methode());
+        objectB.onClick.AddListener(() => showAssetBar(1));
 
         brushB = GameObject.Find("BrushB").GetComponent<Button>();
-        brushB.onClick.AddListener(() => Methode());
+       // brushB.onClick.AddListener(() => );
 
         leftRotateB = GameObject.Find("RotateLeftB").GetComponent<Button>();
-        leftRotateB.onClick.AddListener(() => Methode());
+        leftRotateB.onClick.AddListener(() => osh.rotateLeft());
 
         rightRotateB = GameObject.Find("RotateRightB").GetComponent<Button>();
-        rightRotateB.onClick.AddListener(() => Methode());
+        rightRotateB.onClick.AddListener(() => osh.rotateRight());
 
         removeB = GameObject.Find("RemoveB").GetComponent<Button>();
-        removeB.onClick.AddListener(() => Methode());
+        removeB.onClick.AddListener(() => osh.activateDestroyTool());
+
+        objectPrefabs = new List<GameObject>();
+
+        foreach(GameObject key in LookUpTable.prefabsInverse.Keys)
+        {
+            objectPrefabs.Add(key);
+        }
+
+        objectBar = GameObject.Find("ObjectBar");
+
+        for (int i = 0; i < objectBar.transform.childCount; i++)
+        {
+            GameObject temp = objectBar.transform.GetChild(i).gameObject;
+            temp.GetComponent<Button>().onClick.AddListener(() => osh.activatePlacingTool(objectPrefabs[i]));
+            objectButtons.Add(temp);
+        }
+
+        assetBar = GameObject.Find("Assetbar");
+        assetBar.SetActive(false);
+
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
+
+
+    void showAssetBar(int value)
+    {
+        if (!assetBar.activeSelf)
+        {
+            assetBar.SetActive(true);
+            switch (value)
+            {
+                
+            }
+        }
+    }
 
 
 }
